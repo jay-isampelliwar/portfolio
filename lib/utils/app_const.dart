eab432595ebe5239967c979e855c50cbf2e20cc1
jay-isampelliwar <isampelliwar79@gmail.com>
2023-05-25 17:57:43 +0530

    added

---
+++ lib/utils/app_const.dart
@@ -0,0 +1,4 @@
+class AppConst {
+  static double width = 0.0;
+  static double height = 0.0;
+}
---
+++ lib/utils/web_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class AppColors {
+  static Color primary = Colors.black;
+  static Color secondary = HexColor("##00c5ad");
+}
