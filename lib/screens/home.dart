b03c657ab5267534c57ddb09962080f813981fa7
jay-isampelliwar <isampelliwar79@gmail.com>
2023-05-27 18:16:13 +0530

    removed mediaQuery vari

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,15 +3,11 @@ import 'package:portfolio/responsive.dart';
 import 'package:portfolio/screens/desktop.dart';
 import 'package:portfolio/screens/mobile.dart';
 
-import '../utils/app_const.dart';
-
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    AppConst.width = MediaQuery.of(context).size.width;
-    AppConst.height = MediaQuery.of(context).size.height;
     return const Responsive(
       desktop: Desktop(),
       mobile: MobileScreen(),
