ed8a850a21fdf9167b5e5658f1bd18a9fc9edc9c
jay-isampelliwar <isampelliwar79@gmail.com>
2023-05-27 18:16:25 +0530

    added h1 to h6 styles

--- lib/utils/text_styles.dart
+++ lib/utils/text_styles.dart
@@ -1,13 +1,93 @@
 import 'package:flutter/material.dart';
 
 class AppTextStyles {
-  static TextStyle text12(
+  static TextStyle h1(
       {bool light = false,
       bool bold = false,
-      required double val,
+      required Size size,
       Color color = Colors.white}) {
     return TextStyle(
-      fontSize: val,
+      fontSize: size.width * 0.06,
+      color: color,
+      fontWeight: bold
+          ? FontWeight.bold
+          : light
+              ? FontWeight.w100
+              : FontWeight.normal,
+    );
+  }
+
+  static TextStyle h2(
+      {bool light = false,
+      bool bold = false,
+      required Size size,
+      Color color = Colors.white}) {
+    return TextStyle(
+      fontSize: size.width * 0.05,
+      color: color,
+      fontWeight: bold
+          ? FontWeight.bold
+          : light
+              ? FontWeight.w100
+              : FontWeight.normal,
+    );
+  }
+
+  static TextStyle h3(
+      {bool light = false,
+      bool bold = false,
+      required Size size,
+      Color color = Colors.white}) {
+    return TextStyle(
+      fontSize: size.width * 0.04,
+      color: color,
+      fontWeight: bold
+          ? FontWeight.bold
+          : light
+              ? FontWeight.w100
+              : FontWeight.normal,
+    );
+  }
+
+  static TextStyle h4(
+      {bool light = false,
+      bool bold = false,
+      required Size size,
+      Color color = Colors.white}) {
+    return TextStyle(
+      fontSize: size.width * 0.03,
+      color: color,
+      fontWeight: bold
+          ? FontWeight.bold
+          : light
+              ? FontWeight.w100
+              : FontWeight.normal,
+    );
+  }
+
+  static TextStyle h5(
+      {bool light = false,
+      bool bold = false,
+      required Size size,
+      Color color = Colors.white}) {
+    return TextStyle(
+      fontSize: size.width * 0.02,
+      color: color,
+      fontWeight: bold
+          ? FontWeight.bold
+          : light
+              ? FontWeight.w100
+              : FontWeight.normal,
+    );
+  }
+
+  static TextStyle h6(
+      {bool light = false,
+      bool bold = false,
+      required Size size,
+      Color color = Colors.white}) {
+    return TextStyle(
+      fontSize: size.width * 0.01,
       color: color,
       fontWeight: bold
           ? FontWeight.bold
