e7bd8087a2293057f933d314d81cc20cd44ac034
jay-isampelliwar <isampelliwar79@gmail.com>
2023-05-28 22:07:54 +0530

    added dark and lite primary color

--- lib/utils/web_colors.dart
+++ lib/utils/web_colors.dart
@@ -5,4 +5,6 @@ class AppColors {
   static Color primary = HexColor("#21262d");
   static Color secondary = HexColor("#00c5ad");
   static Color footer = HexColor("#333333");
+  static Color litePrimary = HexColor("#4e5991");
+  static Color darkPrimary = HexColor("#01051d");
 }
