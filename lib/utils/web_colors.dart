ad30b4d12a07926b8beef717ace43016c42756a9
jay-isampelliwar <isampelliwar79@gmail.com>
2023-05-28 17:01:12 +0530

    changes primary color

--- lib/utils/web_colors.dart
+++ lib/utils/web_colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 class AppColors {
-  static Color primary = Colors.black;
+  static Color primary = HexColor("#21262d");
   static Color secondary = HexColor("#00c5ad");
   static Color footer = HexColor("#333333");
 }
