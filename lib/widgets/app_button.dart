5de3ac8b7b35f7af10b486abc42aeba823c72509
jay-isampelliwar <isampelliwar79@gmail.com>
2023-05-28 10:07:45 +0530

    added provider for button

--- lib/widgets/app_button.dart
+++ lib/widgets/app_button.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/provider/button.dart';
+import 'package:provider/provider.dart';
 import '../utils/text_styles.dart';
 
 class AppButton extends StatefulWidget {
@@ -22,57 +24,47 @@ class AppButton extends StatefulWidget {
 }
 
 class _AppButtonState extends State<AppButton> {
-  bool isHover = false;
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return GestureDetector(
-      onTap: widget.onTap,
-      child: MouseRegion(
-        onEnter: (event) => changeState(true),
-        onExit: (event) => changeState(false),
-        child: AnimatedContainer(
-          duration: const Duration(milliseconds: 50),
-          margin: EdgeInsets.symmetric(
-            vertical: size.height * 0.02,
-          ),
-          height: size.width * 0.035,
-          width: size.width * 0.15,
-          decoration: BoxDecoration(
-            color: isHover ? widget.borderColor : widget.bgColor,
-            border: Border.all(color: widget.borderColor, width: 2),
-          ),
-          child: Align(
-            child: Text(
-              widget.text,
-              style: AppTextStyles.h5(bold: true, size: size).copyWith(
-                color: isHover ? widget.replaceTextColor : widget.textColor,
+    return Consumer<ButtonProvider>(
+      builder: (context, buttonProvider, child) {
+        return GestureDetector(
+          onTap: widget.onTap,
+          child: MouseRegion(
+            onEnter: (event) => changeState(true),
+            onExit: (event) => changeState(false),
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 50),
+              margin: EdgeInsets.symmetric(
+                vertical: size.height * 0.02,
+              ),
+              height: size.width * 0.035,
+              width: size.width * 0.15,
+              decoration: BoxDecoration(
+                color: buttonProvider.isHover
+                    ? widget.borderColor
+                    : widget.bgColor,
+                border: Border.all(color: widget.borderColor, width: 2),
+              ),
+              child: Align(
+                child: Text(
+                  widget.text,
+                  style: AppTextStyles.h5(bold: true, size: size).copyWith(
+                    color: buttonProvider.isHover
+                        ? widget.replaceTextColor
+                        : widget.textColor,
+                  ),
+                ),
               ),
             ),
           ),
-        ),
-        // child: isHover
-        //     ? PlainButton(
-        //         text: widget.text,
-        //         bgColor: widget.bgColor,
-        //         borderColor: widget.borderColor,
-        //         textColor: widget.textColor,
-        //         isHover: true,
-        //       )
-        //     : PlainButton(
-        //         isHover: false,
-        //         bgColor: widget.bgColor,
-        //         borderColor: widget.borderColor,
-        //         textColor: widget.textColor,
-        //         text: widget.text,
-        //       ),
-      ),
+        );
+      },
     );
   }
 
   void changeState(bool val) {
-    setState(() {
-      isHover = val;
-    });
+    Provider.of<ButtonProvider>(context, listen: false).setHover(val);
   }
 }
