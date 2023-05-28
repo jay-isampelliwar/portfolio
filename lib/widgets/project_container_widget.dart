3a270a6836cb20640a5711f66c96f43e827743fe
jay-isampelliwar <isampelliwar79@gmail.com>
2023-05-28 22:08:08 +0530

    fixed overflow error

--- lib/widgets/project_container_widget.dart
+++ lib/widgets/project_container_widget.dart
@@ -1,66 +1,133 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/utils/text_styles.dart';
 import 'package:portfolio/widgets/app_button.dart';
 
-import '../utils/text_styles.dart';
 import '../utils/web_colors.dart';
 
-class ProjectContainer extends StatelessWidget {
+class ProjectContainer extends StatefulWidget {
   ProjectContainer(
       {required this.onTap,
       required this.description,
       required this.title,
+      required this.techStack,
       super.key});
 
+  List<String> techStack;
   String title;
   String description;
   Function()? onTap;
+
+  @override
+  State<ProjectContainer> createState() => _ProjectContainerState();
+}
+
+class _ProjectContainerState extends State<ProjectContainer> {
+  bool isHover = false;
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return SizedBox(
-      width: size.width * 0.6,
-      child: FittedBox(
+    return MouseRegion(
+      onEnter: (event) => changeState(true),
+      onExit: (event) => changeState(false),
+      child: Container(
+        width: size.width * 0.6,
+        padding: EdgeInsets.symmetric(
+            vertical: size.width * 0.02, horizontal: size.width * 0.02),
+        decoration: BoxDecoration(
+            color: AppColors.primary,
+            borderRadius: BorderRadius.circular(24),
+            boxShadow: [
+              BoxShadow(
+                  offset: isHover ? const Offset(3, 3) : Offset.zero,
+                  color: AppColors.darkPrimary,
+                  blurRadius: isHover ? 3 : 0),
+              BoxShadow(
+                  offset: isHover ? const Offset(-3, -3) : Offset.zero,
+                  color: AppColors.litePrimary,
+                  blurRadius: isHover ? 3 : 0)
+            ]),
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title,
-                  style: AppTextStyles.h5(size: size, bold: true),
-                ),
-                SizedBox(
-                  height: size.width * 0.01,
-                ),
-                Text(
-                  description,
-                  style: AppTextStyles.h6(
-                    size: size,
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.title,
+                    style: AppTextStyles.h5(
+                      size: size,
+                      bold: true,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: size.width * 0.01,
-                ),
-                AppButton(
-                  onTap: onTap,
-                  textColor: Colors.white,
-                  text: "Source Code",
-                  replaceTextColor: AppColors.primary,
-                ),
-              ],
+                  SizedBox(
+                    height: size.width * 0.02,
+                  ),
+                  Text(
+                    widget.description,
+                    textAlign: TextAlign.justify,
+                    style: AppTextStyles.h6(
+                      size: size,
+                      lite: true,
+                    ),
+                  ),
+                  SizedBox(
+                    height: size.width * 0.02,
+                  ),
+                  Wrap(
+                    children: widget.techStack
+                        .map(
+                          (tech) => Container(
+                            padding: EdgeInsets.all(size.width * 0.004),
+                            margin: EdgeInsets.only(right: size.width * 0.004),
+                            decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.circular(size.width * 0.01),
+                              border: Border.all(
+                                width: 1,
+                                color: AppColors.secondary,
+                              ),
+                            ),
+                            child: Text(
+                              tech,
+                              style: AppTextStyles.h6(
+                                size: size,
+                                lite: true,
+                              ).copyWith(fontSize: size.width * 0.008),
+                            ),
+                          ),
+                        )
+                        .toList(),
+                  ),
+                  SizedBox(
+                    height: size.width * 0.02,
+                  ),
+                  AppButton(
+                    onTap: widget.onTap,
+                    textColor: Colors.white,
+                    text: "Source Code",
+                    replaceTextColor: Colors.black,
+                  )
+                ],
+              ),
             ),
             SizedBox(
-              width: size.width * 0.1,
+              width: size.width * 0.01,
             ),
             Container(
               height: size.width * 0.2,
               width: size.width * 0.2,
-              color: AppColors.secondary,
-            ),
+              color: AppColors.footer,
+            )
           ],
         ),
       ),
     );
+  }
+
+  void changeState(bool val) {
+    setState(() {
+      isHover = val;
+    });
   }
 }
