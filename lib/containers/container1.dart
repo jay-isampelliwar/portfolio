e6be300351f92718864bff0f2abbb0ffbdeaaadc
jay-isampelliwar <isampelliwar79@gmail.com>
2023-05-28 23:04:38 +0530

    added animation in name

--- lib/containers/container1.dart
+++ lib/containers/container1.dart
@@ -5,10 +5,34 @@ import 'package:provider/provider.dart';
 
 import '../utils/text_styles.dart';
 
-class Container1 extends StatelessWidget {
+class Container1 extends StatefulWidget {
   const Container1({
     Key? key,
   }) : super(key: key);
+
+  @override
+  State<Container1> createState() => _Container1State();
+}
+
+class _Container1State extends State<Container1>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    );
+
+    _animationController.repeat(reverse: true);
+    _animation = Tween(begin: 0.0, end: 1.0).animate(_animationController)
+      ..addListener(() {
+        setState(() {});
+      });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,37 +67,47 @@ class Container1 extends StatelessWidget {
                           left: value,
                         ),
                         child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
                           crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.start,
                           children: [
-                            RichText(
-                              text: TextSpan(
-                                children: [
-                                  TextSpan(
-                                    text: "Hi, my name is ",
-                                    style:
-                                        AppTextStyles.h4(bold: true, size: size)
-                                            .copyWith(
-                                      color: Colors.white,
-                                    ),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Hi, my name is ",
+                                  style:
+                                      AppTextStyles.h4(bold: true, size: size)
+                                          .copyWith(
+                                    color: Colors.white,
                                   ),
-                                  TextSpan(
-                                    text: "Jay Isampelliwar\n",
-                                    style: AppTextStyles.h4(
-                                            bold: true, size: size)
-                                        .copyWith(color: AppColors.secondary),
-                                  ),
-                                  TextSpan(
-                                    text: "I'm Flutter Developer.",
-                                    style:
-                                        AppTextStyles.h4(bold: true, size: size)
-                                            .copyWith(
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                ],
-                              ),
+                                ),
+                                ShaderMask(
+                                  shaderCallback: (bounds) {
+                                    return LinearGradient(stops: [
+                                      _animation.value - 0.3,
+                                      _animation.value,
+                                      _animation.value + 0.3
+                                    ], colors: [
+                                      AppColors.secondary,
+                                      AppColors.litePrimary,
+                                      AppColors.footer
+                                    ]).createShader(bounds);
+                                  },
+                                  child: Text("Jay Isampelliwar\n",
+                                      style: AppTextStyles.h4(
+                                              bold: true, size: size)
+                                          .copyWith(
+                                              color: AppColors.secondary)),
+                                ),
+                              ],
                             ),
+                            Text(
+                              "I'm Flutter Developer.",
+                              style: AppTextStyles.h4(bold: true, size: size)
+                                  .copyWith(
+                                color: Colors.white,
+                              ),
+                            )
                           ],
                         ),
                       );
