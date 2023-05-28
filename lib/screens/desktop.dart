f1b0e73b854204c1729e3ff81d8d5ddb96c3e9c8
jay-isampelliwar <isampelliwar79@gmail.com>
2023-05-28 13:19:51 +0530

    uncommetns

--- lib/screens/desktop.dart
+++ lib/screens/desktop.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:portfolio/containers/container4.dart';
+
+import '../containers/container1.dart';
+import '../containers/container2.dart';
+import '../containers/container3.dart';
+import '../containers/container4.dart';
+import '../containers/footer.dart';
 
 class Desktop extends StatelessWidget {
   const Desktop({Key? key}) : super(key: key);
@@ -12,10 +17,11 @@ class Desktop extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: const [
-            // Container1(),
-            // Container2(),
+            Container1(),
+            Container2(),
+            Container3(),
             Container4(),
-            // Footer(),
+            Footer(),
           ],
         ),
       ),
