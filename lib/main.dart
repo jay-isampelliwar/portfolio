c2d42aa85292e2a3cb6cff449c78d3d07849f7c6
jay-isampelliwar <isampelliwar79@gmail.com>
2023-05-28 10:07:25 +0530

    added provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/provider/button.dart';
 import 'package:portfolio/screens/home.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,14 +13,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: "Montserrat",
-        primaryColor: Colors.greenAccent,
-        brightness: Brightness.light,
-      ),
-      home: const Home(),
-    );
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (context) => ButtonProvider())
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            fontFamily: "Montserrat",
+            primaryColor: Colors.greenAccent,
+            brightness: Brightness.light,
+          ),
+          home: const Home(),
+        ));
   }
 }
