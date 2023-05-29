333320aee127093a0bd00b2412ba00dbf723da08
jay-isampelliwar <isampelliwar79@gmail.com>
2023-05-29 15:30:47 +0530

    added two gradient colors

--- lib/utils/web_colors.dart
+++ lib/utils/web_colors.dart
@@ -3,8 +3,11 @@ import 'package:hexcolor/hexcolor.dart';
 
 class AppColors {
   static Color primary = HexColor("#21262d");
-  static Color secondary = HexColor("#00c5ad");
+  static Color secondary = HexColor("#D36DFF");
+  // static Color secondary = HexColor("#00c5ad");
   static Color footer = HexColor("#333333");
   static Color litePrimary = HexColor("#4e5991");
   static Color darkPrimary = HexColor("#01051d");
+  static Color gradientColor1 = HexColor("#84D2FF");
+  static Color gradientColor2 = HexColor("#D36DFF");
 }
