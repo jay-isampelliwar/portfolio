6a78f1f9fa5be9537e21c4ebf243bc38a9d5ce99
jay-isampelliwar <isampelliwar79@gmail.com>
2023-05-29 15:30:28 +0530

    added summary and educations blocks

--- lib/containers/container2.dart
+++ lib/containers/container2.dart
@@ -1,11 +1,6 @@
-import 'dart:html' as html;
-
 import 'package:flutter/material.dart';
 
 import 'package:portfolio/utils/text_styles.dart';
-import 'package:portfolio/utils/web_colors.dart';
-
-import '../widgets/app_button.dart';
 
 class Container2 extends StatelessWidget {
   const Container2({super.key});
@@ -15,12 +10,12 @@ class Container2 extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return Container(
       width: size.width,
-      color: AppColors.primary,
+      margin: EdgeInsets.symmetric(
+        vertical: size.width * 0.04,
+      ),
+      padding: EdgeInsets.symmetric(horizontal: size.width * 0.04),
       child: Column(
         children: [
-          SizedBox(
-            height: size.height * 0.03,
-          ),
           Text(
             "ABOUT ME",
             style: AppTextStyles.h4(
@@ -29,38 +24,52 @@ class Container2 extends StatelessWidget {
             ),
           ),
           SizedBox(
-            height: size.height * 0.01,
+            height: size.width * 0.04,
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                height: size.width * 0.2,
-                width: size.width * 0.2,
-                color: AppColors.primary,
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "SUMMARY",
+                      style: AppTextStyles.h5(size: size),
+                    ),
+                    SizedBox(
+                      height: size.width * 0.02,
+                    ),
+                    Text(
+                      "I am a junior Flutter developer, passionate about creating innovative and engaging mobile applications. With a strong foundation in programming and a keen interest in mobile development, I have embarked on a journey to master the Flutter framework.",
+                      textAlign: TextAlign.justify,
+                      style: AppTextStyles.h6(size: size),
+                    ),
+                  ],
+                ),
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "I am a junior Flutter developer, passionate about creating innovative and \nengaging mobile applications. With a strong foundation in programming \nand a keen interest in mobile development, I have embarked on a journey \nto master the Flutter framework.",
-                    style: AppTextStyles.h6(size: size),
-                  ),
-                  SizedBox(
-                    height: size.height * 0.01,
-                  ),
-                  AppButton(
-                    text: "View Resume",
-                    replaceTextColor: Colors.black,
-                    textColor: Colors.white,
-                    onTap: () {
-                      html.window.open(
-                        "https://drive.google.com/file/d/1n9Y-W5iSe452Va1wgtSO_1u9Bl4_umKn/view?usp=share_link",
-                        "_blank",
-                      );
-                    },
-                  )
-                ],
+              SizedBox(
+                width: size.width * 0.04,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "EDUCATION",
+                      style: AppTextStyles.h5(size: size),
+                    ),
+                    SizedBox(
+                      height: size.width * 0.02,
+                    ),
+                    Text(
+                      "I recently completed B.Tech degree from Rajiv Gandhi College of Engineering Research and Technology. I specialized in Computer Science Engineering and gained hands-on experience through projects and coursework.I look forward to utilizing my education and experiences to tackle challenges, drive innovation, and contribute to the ever-evolving world of technology.",
+                      textAlign: TextAlign.justify,
+                      style: AppTextStyles.h6(size: size),
+                    ),
+                  ],
+                ),
               ),
             ],
           )
