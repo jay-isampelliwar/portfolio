2e4403ac10a0d5e92056bbd9403dd655a28ad41b
jay-isampelliwar <isampelliwar79@gmail.com>
2023-06-01 12:42:24 +0530

    delete unwanted colors

--- lib/utils/web_colors.dart
+++ lib/utils/web_colors.dart
@@ -3,11 +3,8 @@ import 'package:hexcolor/hexcolor.dart';
 
 class AppColors {
   static Color primary = HexColor("#0A0A0A");
-  static Color secondary = HexColor("#D36DFF");
-  // static Color secondary = HexColor("#00c5ad");
   static Color footer = HexColor("#333333");
-  static Color litePrimary = HexColor("#4e5991");
-  static Color darkPrimary = HexColor("#01051d");
-  static Color gradientColor1 = HexColor("#84D2FF");
-  // static Color gradientColor1 = const Color(0xFFFFEE00);
+
+  // static Color secondary = HexColor("#84D2FF");
+  static Color secondary = const Color(0xFFFFEE00);
 }
